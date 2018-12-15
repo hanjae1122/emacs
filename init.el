b88ab524054899b34383d958c0cf8a932791b8f0
John Han <han.jae1122@gmail.com>
2018-12-15 00:10:27 -0500

    Use split-and-follow keybinding for window splitting

--- init.el
+++ init.el
@@ -59,6 +59,22 @@
 ;; delete the selection with a keypress
 (delete-selection-mode t)
 
+;; Key bindings
+(defun split-and-follow-horizontally ()
+  (interactive)
+  (split-window-below)
+  (balance-windows)
+  (other-window 1))
+(global-set-key (kbd "C-x 2") 'split-and-follow-horizontally)
+
+(defun split-and-follow-vertically ()
+  (interactive)
+  (split-window-right)
+  (balance-windows)
+  (other-window 1))
+(global-set-key (kbd "C-x 3") 'split-and-follow-vertically)
+
+
 ;; ;; Package install settings
 ;; ;; add additional trusted CAs to allow gnu, melpa
 ;; (require 'gnutls)
